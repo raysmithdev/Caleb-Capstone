c56332eeff7fd36d91b6d23898dc314c03df88dc
Caleb Blackwell <calebxblackwell@gmail.com>
2017-12-20 15:19:31 -0800

    second commit

--- Public/server.js/server.js
+++ Public/server.js/server.js
@@ -3,10 +3,4 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-if (require.main === module) {
-  app.listen(process.env.PORT || 8080, function () {
-    console.info(`App listening on ${this.address().port}`);
-  });
-}
-
-module.exports = app;
+app.listen(process.env.PORT || 8080);
